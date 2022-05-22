2b454a5cb2422b79b91c00ef55155033d54de275
Brendson <brendson.net@gmail.com>
2022-05-22 18:47:03 -0300

    feat: created the routes to delete and change person

--- src/main/java/br/com/springboot/server/rh/controller/PersonController.java
+++ src/main/java/br/com/springboot/server/rh/controller/PersonController.java
@@ -1,10 +1,13 @@
 package br.com.springboot.server.rh.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.springboot.server.rh.domain.Person;
@@ -30,6 +33,30 @@ public class PersonController {
 
     return "rh/people/form";
   }
+
+  @GetMapping("/rh/pessoas/{id}")
+  public String changePerson(@PathVariable("id") Long id, Model model){
+    Optional<Person> person = personRepository.findById(id);
+    if(person.isEmpty()) {
+       throw new IllegalArgumentException("invalid person...");
+    }
+
+    model.addAttribute("person", person.get());
+
+    return "rh/people/form";
+  }
+
+  @GetMapping("/rh/pessoas/excluir/{id}")
+  public String deletePerson(@PathVariable("id") Long id, Model model) {
+   Optional<Person> person = personRepository.findById(id);
+   if(person.isEmpty()){
+      throw new IllegalArgumentException("Pessoa inválida.");
+   }
+
+   personRepository.delete(person.get());
+
+   return"redirect:/rh/pessoas";
+   }
 
   @PostMapping("/rh/pessoas/salvar")
   public String savePerson(@ModelAttribute("person") Person person){
